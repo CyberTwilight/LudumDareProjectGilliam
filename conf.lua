1f254de395b85f1a250fca904ac2c0cc6ffaa051
nikopaes <nickxp51@hotmail.com>
2016-04-16 19:20:30 -0300

    -Added background sprite -Added map folder for all map.lua files -Added background.lua, all map focused codes here -Called functions that draws map in main (not the best way, maybe)

---
+++ conf.lua
@@ -0,0 +1,14 @@
+--Configuração
+function love.conf(t)
+  
+  
+    t.title = "PROJECT GILLIGAN" 
+    t.version  = "0.10.1"
+    --definiçao da area da janela
+    t.window.width = 1366
+    t.window.height = 768
+    
+    
+    --para debugging do Windows lembrar de mudar para false para o release
+    t.console = true
+  end
---
+++ map/background.lua
@@ -0,0 +1,16 @@
+local map = {}
+
+function map.load()
+    background = love.graphics.newImage("assets/background.jpg")
+  
+end
+function map.update(dt)
+  
+end
+function map.draw()
+    love.graphics.draw(background,0,0)
+    
+    
+
+end
+return map
