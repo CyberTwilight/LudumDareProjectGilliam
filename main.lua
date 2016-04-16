b94a67476d78d0b04e8c06c0ee138183a8c76098
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2016-04-16 19:42:45 +0200

    initial

---
+++ main.lua
@@ -0,0 +1,18 @@
+local intro = require "src/intro"
+local menu = require "src/menu"
+local game = require "src/game"
+function love.load()
+    scenes = { intro = intro, menu = menu, game = game}
+    change_scene("intro")
+end
+function change_scene(new)
+    scene = new
+    scenes[scene].load()
+end
+function love.update(dt)
+    scenes[scene].update(dt)
+end
+
+function love.draw()
+    scenes[scene].draw()
+end
